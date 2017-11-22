50871e22b9e68c7326c3b2241e368f94aecf55d2
Viko@raspberry <victor@stationf.co>
2017-11-22 23:58:18 +0100

    Fix

--- Colleen.c
+++ Colleen.c
@@ -1,3 +1,9 @@
-/*commentaire1*/
+/*
+	commentaire1
+*/
 #include <stdio.h>
-void func(){};int main(){/*commentaire2*/char *c="/*commentaire1*/%c#include <stdio.h>%cvoid func(){};int main(){/*commentaire2*/char *c=%c%s%c;printf(c,10,10,34,c,34,10);func();}%c";printf(c,10,10,34,c,34,10);func();}
+void func(){};int main(){
+/*
+	commentaire2
+*/
+char *c="/*%1$c%4$ccommentaire1%1$c*/%1$c#include <stdio.h>%1$cvoid func(){};int main(){%1$c/*%1$c%4$ccommentaire2%1$c*/%1$cchar *c=%2$c%s%2$c;printf(c,10,34,c,9);func();}%1$c";printf(c,10,34,c,9);func();}
