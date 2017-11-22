7632badf340e4140a573aa207c31971c66869cbb
Viko@raspberry <victor@stationf.co>
2017-11-23 00:39:59 +0100

    Fix

--- Grace.c
+++ Grace.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+/*
+	commentaire
+*/
 #define FP
 #define FW
-#define FU() int main(){FILE*f = fopen("Grace_kid.c", "w");char *c="#include <stdio.h>%c#define FP%c#define FW%c#define FU() int main(){FILE*f = fopen(%cGrace_kid.c%c, %cw%c);char *c=%c%s%c;fprintf(f,c,10,10,10,34,34,34,34,34,c,34,10,10);}%cFU();%c";fprintf(f,c,10,10,10,34,34,34,34,34,c,34,10,10);}
-FU();
+#define FU(x) int main(){FILE*f = fopen("Grace_kid.c", "w");char *c="#include <stdio.h>%1$c/*%1$c%4$ccommentaire%1$c*/%1$c#define FP%1$c#define FW%1$c#define FU(x) int main(){FILE*f = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);char *c=%2$c%3$s%2$c;fprintf(f,c,10,34,c,9);}%1$cFU(1);%1$c";fprintf(f,c,10,34,c,9);}
+FU(1);
