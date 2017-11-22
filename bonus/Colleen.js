6d3d7915982f667580ed2f080cf058df3589d7ac
Viko@raspberry <victor@stationf.co>
2017-11-23 00:46:22 +0100

    Fix

--- bonus/Colleen.js
+++ bonus/Colleen.js
@@ -1,4 +1,6 @@
-/*commentaire*/
-var func = function () { return "/*commentaire2*/mafunction" }
-var f = function () { return "/*commentaire*/\nvar func = " + func.toString() + "\nvar f = " + f.toString() + "\nconsole.log(f());" }
+/*
+	commentaire
+*/
+var func = function () { return "/*\n\tcommentaire2\n*/\n" }
+var f = function () { return "/*\n\tcommentaire\n*/\nvar func = " + func.toString() + "\nvar f = " + f.toString() + "\nconsole.log(f());" }
 console.log(f());
--- bonus/Grace.js
+++ bonus/Grace.js
@@ -1,3 +1,6 @@
 var fs = require('fs');
-const f = function () { fs.writeFile("./Grace_kid.js", "var fs = require('fs');\nconst f = " + f.toString() + "\nf();", function (err) {if (err) console.log(err);}) }
+/*
+	commentaire
+*/
+const f = function () { fs.writeFile("./Grace_kid.js", "var fs = require('fs');\n/*\n\tcommentaire\n*/\nconst f = " + f.toString() + "\nf();\n", function (err) {if (err) console.log(err);}) }
 f();
